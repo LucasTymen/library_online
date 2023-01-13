22f527c6620da6d867636dac9917e647276ab884
Lucas Tymen <lucas.tymen@gmail.com>
2023-01-13 23:50:51 +0100

    push navb

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module LibraryOnline
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = 'Paris (France)'
+    config.time_zone = "Europe/Paris"
 
     config.i18n.default_locale = :fr
     # config.eager_load_paths << Rails.root.join("extras")
